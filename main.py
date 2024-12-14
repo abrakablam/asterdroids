83b0b353f89e0e5ebb406ec30fb1578b9c09498c
abrakablam <165574894+abrakablam@users.noreply.github.com>
2024-12-14 16:41:31 -0500

    adding players

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+<<<<<<< HEAD
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+=======
+from player import Player
+>>>>>>> 2188b51 (adding players)
 
 
 def main():
     pygame.init()
+<<<<<<< HEAD
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +19,27 @@ def main():
         screen.fill((0, 0, 0))
         pygame.display.flip()
 
+=======
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))  # black RGB values
+
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limiting the framerate to 60 fps
+        dt = clock.tick(60) / 1000
+
+>>>>>>> 2188b51 (adding players)
 
 if __name__ == "__main__":
     main()
