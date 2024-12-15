cf35610749ec4d8f93e32baf5734d1a4d8fb596d
abrakablam <165574894+abrakablam@users.noreply.github.com>
2024-12-14 19:30:04 -0500

    Added sprite movement for player

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 import pygame
 from constants import *
-<<<<<<< HEAD
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-=======
 from player import Player
->>>>>>> 2188b51 (adding players)
 
 
 def main():
     pygame.init()
-<<<<<<< HEAD
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    while True:
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
-
-=======
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -31,6 +16,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
         screen.fill((0, 0, 0))  # black RGB values
 
         player.draw(screen)
@@ -39,7 +25,6 @@ def main():
         # limiting the framerate to 60 fps
         dt = clock.tick(60) / 1000
 
->>>>>>> 2188b51 (adding players)
 
 if __name__ == "__main__":
     main()
