f4fffcff573386763df8143f69347c5834463866
abrakablam <165574894+abrakablam@users.noreply.github.com>
2024-12-14 20:20:37 -0500

    added basic asteroid behavior and spawning

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     # these are pygame 'groups'
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    dt = 0
+
     # game loop
     while True:
         for event in pygame.event.get():
@@ -22,12 +30,12 @@ def main():
                 return
 
         for obj in updatable:
-            player.update(dt)
+            obj.update(dt)
 
         screen.fill((0, 0, 0))  # black RGB values
 
         for obj in drawable:
-            player.draw(screen)
+            obj.draw(screen)
 
         pygame.display.flip()
 
